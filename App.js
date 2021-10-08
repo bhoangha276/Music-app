4a051eac966b478692594576a38f9d1f5f309334
HaHoang <82991310+bhoangha276@users.noreply.github.com>
2021-10-08 22:15:36 +0700

    ha

--- App.js
+++ App.js
@@ -1,25 +1,142 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './screens/HomeScreen';
-import DetailScreen from './screens/DetailScreen';
-const Stack = createNativeStackNavigator();
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+  StatusBar,
+  StyleSheet,
+  Image,
+  Animated,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+} from 'react-native';
+import {NavigationContainer, DrawerActions} from '@react-navigation/native';
+// import {createStackNavigator} from '@react-navigation/stack';
+// import * as Animatable from 'react-native-animatable';
+// import {Surface} from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Icon2 from 'react-native-vector-icons/Ionicons';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-export default function App() {
+// import Tabs from './App/config/router';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  // goToTabs() {
+  //   this.props.navigation.navigate('Tabs');
+  // }
+
+  render() {
+    // const zoomIn = {
+    //   0: {
+    //     scale: 0,
+    //   },
+    //   0.5: {
+    //     scale: 0.5,
+    //   },
+    //   1: {
+    //     scale: 1,
+    //   },
+    // };
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="dark-content" backgroundColor="#fff" />
+        <Text style={styles.title}>Music App</Text>
+        <Image
+          
+          source={require('./App/Assets/logo.png')}
+          style={styles.logo}
+        />
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.text}>Start Listening</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+export default App;
+
+function Stacks() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="App"
+        component={App}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="Tabs"
+        component={Tabs}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="Player"
+        component={Player}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="Details"
+        component={CategoriesDetails}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function MainScreen() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name='Home'
-          component={HomeScreen}
-          options={{title: 'Welcome'}}
-        ></Stack.Screen>
-        <Stack.Screen
-          name='Detail'
-          component={DetailScreen}
-        ></Stack.Screen>
-      </Stack.Navigator>      
+      <Stacks />
     </NavigationContainer>
   );
 }
+
+// export default MainScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 32,
+    color: '#000',
+    fontWeight: 'bold',
+  },
+  logo: {
+    height: 220,
+    width: '80%',
+    marginBottom: 40,
+    marginTop: 20,
+  },
+  btn: {
+    width: '60%',
+    height: 50,
+    borderRadius: 20,
+    backgroundColor: '#ff5b77',
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
+    elevation: 15,
+  },
+  text: {
+    color: '#fff',
+    fontSize: 20,
+  },
+});
+
+
---
+++ App/Screens/Home.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {View} from 'react-native';
+
+class Home extends Component {
+    state = { }
+    render() {
+        return (<View style={StyleSheet.container}></View>)
+    }
+}
+
+export default Home;
+
+const styles = StyleSheet.create({
+    container: {
+
+    },
+});
